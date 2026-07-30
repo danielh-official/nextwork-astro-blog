012a38f74607f6f1b5f7133e29c535e2084a3f51
Daniel (Haven) Hotyanov <49914607+danielh-official@users.noreply.github.com>
2026-07-29 20:37:47 -0400

    Move content.config.ts to right location

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,8 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
-import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
